75850d6364c4a040949b1185bf6cb912a5cfef72
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-13 16:39:46 +0600

    fijnsh

--- js/flower_details.js
+++ js/flower_details.js
@@ -1,4 +1,3 @@
-//flower details api fetch
 document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const flowerId = urlParams.get("id");
@@ -11,21 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error("Error fetching flower details:", error));
 });
 
-function order_flower(flower) {
+function orderFlower(flower) {
   const userId = localStorage.getItem("user_id");
   const button = document.getElementById("order_submit");
 
   button.addEventListener("click", () => {
     const input = document.getElementById("quantity");
-    const product_quantity = parseInt(input.value);
+    const productQuantity = parseInt(input.value);
 
     if (!userId) {
-      console.log("User not logged in");
       alert("You need to be logged in to place an order");
       return;
     }
 
-    if (product_quantity <= flower.stock) {
+    if (productQuantity <= flower.stock) {
       fetch("https://django-final-exam-backend-part.onrender.com/orders/create_order/", {
         method: "POST",
         headers: {
@@ -34,121 +32,97 @@ function order_flower(flower) {
         body: JSON.stringify({
           user_id: parseInt(userId),
           product_id: flower.id,
-          quantity: product_quantity,
+          quantity: productQuantity,
         }),
       })
         .then((response) => response.json())
-        .then((response) => {
-          console.log("Order placed successfully:", response);
+        .then(() => {
           alert("Order placed successfully!");
           window.location.href = "./update_profile.html";
         })
-        .catch((error) => {
-          console.log("Order error", error);
+        .catch(() => {
           alert("Error placing order");
           window.location.href = "./update_profile.html";
         });
     } else {
-      console.log("Insufficient stock");
       alert("Insufficient stock");
     }
   });
 }
 
-//flower details dynamic
 function displayFlowerDetails(flower) {
   const detailsContainer = document.getElementById("flower-details");
   detailsContainer.innerHTML = `
-        <div class="card container bg-white text-dark index_flower_card" style="border-radius: 10px;">
-            <br>
-            <img src="${flower.image}" class="card-img-top mx-auto d-block" alt="${flower.title}" style="width:800px; height:500px; border-radius: 10px;">
-            <h1 class="pt-5">${flower.title}</h1>
-            <p>Price : ${flower.price} ৳</p>
-            <p>Category : <span class="btn btn-secondary">${flower.category}</span></p>
-            <p>Stock : ${flower.stock}</p>
-            <p>Description : ${flower.description}</p>   
-            <br>
-            <div class="d-flex gap-3">
-                <div>
-                    <a href="./profile.html" class="btn btn-success text-white">Back To Profile</a>
-                </div>
-               <div>
-                  <!-- Button trigger modal -->
-                  <div class="d-flex gap-3">
-                     <div>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#orderModal">
-                         Order Now
-                        </button>
-                     </div>
-                         <div>
-                            <p>
-                            <button class="btn btn-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                              Comment
-                            </button>
-                             </p>
-                          <div class="collapse" id="collapseExample">
-                            <div class="card-body">
-                                <section>
-                                <div class="comment-section container card bg-white index_flower_card" id="index_flower_card"
-                                  style="border-radius: 10px;">
-                                  <div id="commentForm" class="row g-3 mx-auto w-100 gap-3" style="padding-top: 30px">
-                                    <div class="col-md-12">
-                                      <label for="name" class="form-label"><b>Name</b></label>
-                                      <input type="text" class="form-control" id="name" name="name" required />
-                                    </div>
-                                    <div class="col-md-12">
-                                      <label for="text" class="form-label"><b>Messages</b></label>
-                                      <textarea class="form-control" id="text" name="text" required></textarea>
-                                    </div>
-                                    <div class="col-12">
-                                      <button type="submit" class="btn btn-outline-info" id="submit_buttons">
-                                        Submit
-                                      </button>
-                                    </div>
-                                    <br />
-                                  </div>
-                                </div>
-                                
-                              </section>
-                            </div>
-                          </div>
-                     </div>
-                  </div>
-                  <!-- Modal -->
-                  <div class="modal fade" id="orderModal" tabindex="-1" aria-labelledby="orderModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content" style="width:600px; height:220px;">
-                        <div class="">
-                          <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close" style="padding-left:1150px;"></button>
-                        </div>
-                        <div class="modal-body">
-                          <div class="mb-3">
-                              <label for="quantity" class="form-label text-dark"><b>Quantity</b></label>
-                              <input id="quantity" type="number" class="form-control">
-                          </div>
-                          <button type="button" class="btn btn-primary pl-5" id="order_submit">Submit</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          <br>
+    <div class="card container bg-white text-dark index_flower_card" style="border-radius: 10px;">
+        <br/>
+        <img src="${flower.image}" class="card-img-top mx-auto d-block" alt="${flower.title}" style="width:800px; height:500px; border-radius: 10px;">
+        <h1 class="pt-5">${flower.title}</h1>
+        <p>Price : ${flower.price} ৳</p>
+        <p>Category : <span class="btn btn-secondary">${flower.category}</span></p>
+        <p>Stock : ${flower.stock}</p>
+        <p>Description : ${flower.description}</p>
+        <div class="d-flex gap-3">
+            <a href="./profile.html" class="btn btn-success text-white">Back To Profile</a>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#orderModal">Order Now</button>
+            <button class="btn btn-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample">Comment</button>
         </div>
-    `;
-  order_flower(flower);
-  post_comment(flower.id);
-  get_comments(flower.id);
+        <br/>
+        <div class="collapse" id="collapseExample">
+            <div class="card-body">
+                <section class="container w-75">
+                    <div class="comment-section container w-75 card bg-white index_flower_card" style="border-radius: 10px;">
+                        <div id="commentForm" class="row g-3 mx-auto w-100 gap-3" style="padding-top: 30px">
+                            <div class="col-md-12">
+                                <label for="name" class="form-label"><b>Name</b></label>
+                                <input type="text" class="form-control" id="name" name="name" required />
+                            </div>
+                            <div class="col-md-12">
+                                <label for="text" class="form-label"><b>Messages</b></label>
+                                <textarea class="form-control" id="text" name="text" required></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-outline-info" id="submit_buttons">Submit</button>
+                            </div>
+                          <br/>
+                        </div>
+                    </div>
+                </section>
+              <br/>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="orderModal" tabindex="-1" aria-labelledby="orderModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content" style="width:600px; height:220px;">
+                <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close" style="padding-left:1150px;"></button>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="quantity" class="form-label text-dark"><b>Quantity</b></label>
+                        <input id="quantity" type="number" class="form-control">
+                    </div>
+                    <button type="button" class="btn btn-primary pl-5" id="order_submit">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+  orderFlower(flower);
+  postComment(flower.id);
+  getComments(flower.id);
 }
 
-//comment part
-const post_comment = (flowerId) => {
-  const comment_button = document.getElementById("submit_buttons");
-  comment_button.addEventListener("click", (event) => {
+const postComment = (flowerId) => {
+  const commentButton = document.getElementById("submit_buttons");
+  if (!commentButton) {
+    console.error("Submit button not found");
+    return;
+  }
+  commentButton.addEventListener("click", (event) => {
     event.preventDefault();
+    location.reload();
 
     const username = document.getElementById("name").value;
-    const usertext = document.getElementById("text").value;
+    const userText = document.getElementById("text").value;
 
     fetch("https://django-final-exam-backend-part.onrender.com/flowers/comments_api/", {
       method: "POST",
@@ -158,65 +132,53 @@ const post_comment = (flowerId) => {
       body: JSON.stringify({
         flowerId: flowerId,
         names: username,
-        comment: usertext,
+        comment: userText,
       }),
     })
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Network response was not o k");
         }
         return res.json();
       })
-      .then((resData) => {
-        console.log(resData);
+      .then(() => {
         location.reload();
       })
       .catch((error) => {
-        console.error(error);
+        console.error("Error posting comment:", error);
       });
-
-    console.log(flowerId, username, usertext);
   });
 };
 
-
-const get_comments = (flowerId) => {
+const getComments = (flowerId) => {
   fetch(`https://django-final-exam-backend-part.onrender.com/flowers/get_comment/${flowerId}/`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       displayComment(data);
     });
 };
 
-//comment display
 const displayComment = (comments) => {
-  console.log(comments);
   const commentCount = document.getElementById("comments-count");
-  const commentdiv = document.getElementById("comments-list");
+  const commentDiv = document.getElementById("comments-list");
 
   commentCount.innerHTML = `${comments.length}`;
 
   let commentsHtml = comments.map(comment => `
     <div class="bg-white text-dark card p-4 mb-3 w-25 index_flower_card" style="border-radius: 10px;">
-      <h4>${comment.name}</h4> 
+      <h6>${comment.name}</h6> 
       <p>${comment.body}</p>
       <small>${new Date(comment.created_on).toLocaleString()}</small>
-      <br>
-     <div class="d-flex gap-5">
-        <div>
-            <a class="btn btn-success edit-comment" data-id="${comment.id}" data-name="${comment.name}" data-body="${comment.body}">Edit</a>
-        </div>
-        <div>
-          <a class="btn btn-danger delete-comment" data-id="${comment.id}">Delete</a>
-        </div>    
+      <br/>
+      <div class="d-flex gap-5">
+        <button class="btn btn-success edit-comment" data-id="${comment.id}" data-name="${comment.name}" data-body="${comment.body}">Edit</button>
+        <button class="btn btn-danger delete-comment" data-id="${comment.id}">Delete</button>
       </div>   
     </div>
   `).join('');
-  commentdiv.innerHTML = commentsHtml;
+  commentDiv.innerHTML = commentsHtml;
 };
 
-//comment edit
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("authToken");
   document.getElementById("comments-list").addEventListener("click", (event) => {
@@ -234,7 +196,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.getElementById("edit-comment-form").addEventListener("submit", async (event) => {
     event.preventDefault();
-    location.reload();
     const commentId = document.getElementById("edit-comment-id").value;
     const commentName = document.getElementById("edit-comment-name").value;
     const commentBody = document.getElementById("edit-comment-body").value;
@@ -250,53 +211,41 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       if (response.ok) {
-        document.getElementById("edit-comment-form").style.display = "none";
-        const updatedComments = await get_comments();
+        const updatedComments = await getComments();
         displayComment(updatedComments);
+        document.getElementById("edit-comment-form").style.display = "none";
       } else {
         const errorData = await response.json();
-        console.error("Failed to update comment: ", errorData);
         alert(`Failed to update comment: ${errorData.detail || "Unknown error"}`);
       }
     } catch (error) {
-      console.error("Error: ", error);
-      alert("An error occurred while updating the comment");
+      console.error("Error updating comment:", error);
     }
   });
-});
 
+  document.getElementById("comments-list").addEventListener("click", async (event) => {
+    if (event.target.classList.contains("delete-comment")) {
+      const commentId = event.target.getAttribute("data-id");
 
-//delete comment
-document.addEventListener('DOMContentLoaded', () => {
-  const token = localStorage.getItem("authToken");
-  document.getElementById('comments-list').addEventListener('click', async (event) => {
-    if (event.target.classList.contains('delete-comment')) {
-      const commentId = event.target.getAttribute('data-id');
-      try {
-        if (confirm('Are you sure you want to delete this comment?')) {
-
+      if (confirm("Are you sure you want to delete this comment?")) {
+        try {
           const response = await fetch(`https://django-final-exam-backend-part.onrender.com/flowers/comments/${commentId}/`, {
-            method: 'DELETE',
+            method: "DELETE",
             headers: {
-              'Content-Type': 'application/json',
               Authorization: `token ${token}`,
-            }
+            },
           });
 
           if (response.ok) {
-            const commentElement = event.target.closest('.card');
-            commentElement.remove();
-            const updatedComments = get_comments();
+            const updatedComments = await getComments();
             displayComment(updatedComments);
           } else {
-            alert('Failed to delete comment');
+            alert("Failed to delete comment.");
           }
+        } catch (error) {
+          console.error("Error deleting comment:", error);
         }
-      } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred while deleting the comment');
-        location.reload();
       }
     }
   });
-}); 
+});
